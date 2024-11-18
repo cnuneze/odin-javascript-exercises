45844d788edcca87e765d735431496bd3ea5eff4
cnuneze <23690843+cnuneze@users.noreply.github.com>
2024-11-18 17:52:51 -0300

    Fix: Solve exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayNumber, ...excludedValues) {
+    return arrayNumber.filter((number) => {
+        for (const excludeValue of excludedValues) {
+            if (number === excludeValue) {
+                return false;
+            }
+        }
+        return true;
+    });
 };
 
 // Do not edit below this line
