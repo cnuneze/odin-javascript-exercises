c5970cac542eaed74fccd6b165372151094de5fd
cnuneze <23690843+cnuneze@users.noreply.github.com>
2024-11-18 17:29:41 -0300

    Fix: Add solved exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    
+    let stringLastIndex = string.length - 1;
+    let reversedString = '';
 
+    for (let i = stringLastIndex; i >= 0 ; i--) {
+        reversedString = reversedString.concat(string[i]);
+    }
+
+    return reversedString;
 };
 
 // Do not edit below this line
