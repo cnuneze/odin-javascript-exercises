1349f32915acc2f2a1de1f2fc5239bebab11b71f
cnuneze <23690843+cnuneze@users.noreply.github.com>
2024-11-18 17:08:08 -0300

    Fix: Add solved exercise 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+    isNegative = num < 0;
+    isNotNumber = isNaN(num)
+    if (isNegative || isNotNumber) {
+        return 'ERROR';
+    }
+
+    let repeatedString = '';
+    for (let i = 0; i < num; i++) {
+        repeatedString = repeatedString.concat(string);
+    }
+
+    return repeatedString;
 };
 
 // Do not edit below this line
