b3953b41b9f39b4e14ddd498b2cb155664d8574d
cnuneze <23690843+cnuneze@users.noreply.github.com>
2024-11-18 22:27:21 -0300

    Fix: Solve exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,29 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  
+  if (Number.isNaN(temperature)) {
+    return 'ERROR';
+  }
+
+  let celsius = (temperature - 32) * (5 / 9);
+
+  let preRoundCelsius = celsius * 10;
+  let roundCelsius = (Math.round(preRoundCelsius)) / 10;
+
+  return roundCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+
+  if (Number.isNaN(temperature)) {
+    return 'ERROR';
+  }
+
+  let fahrenheit = (temperature * 9 / 5 + 32);
+
+  let preRoundFahrenheit = fahrenheit * 10;
+  let roundFahrenheit = (Math.round(preRoundFahrenheit)) / 10;
+
+  return roundFahrenheit;
 };
 
 // Do not edit below this line
