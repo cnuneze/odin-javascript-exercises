e0e8a6f89f4caa184782fcbfc4b84ed03660085c
cnuneze <23690843+cnuneze@users.noreply.github.com>
2024-11-18 21:53:14 -0300

    Fix: Solve exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,24 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    if (!Number.isInteger(year)) {
+        return 'ERROR';
+    }
+
+    let isDivisibleBy4 = (year % 4 === 0);
+    let isDivisibleBy100 = (year % 100 === 0);
+    let isDivisibleBy400 = (year % 400 === 0);
+
+    if (isDivisibleBy4) {
+        if (isDivisibleBy100) {
+            if (isDivisibleBy400) {
+                return true;
+            }
+            return false;
+        }
+        return true
+    }
+    
+    return false;
 };
 
 // Do not edit below this line
