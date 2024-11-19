f51ce7eddd59cf9bd040ccd25391fa3a2000d097
cnuneze <23690843+cnuneze@users.noreply.github.com>
2024-11-18 21:15:33 -0300

    Fix: Solve exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
 
+    if (
+        !Number.isInteger(firstNumber) ||
+        !Number.isInteger(secondNumber) ||
+        firstNumber < 0 ||
+        secondNumber < 0
+    ) {
+        return 'ERROR';
+    }
+    let switchValues = firstNumber > secondNumber;
+    let minorNumber = (switchValues) ? secondNumber : firstNumber;
+    let higherNumber = (switchValues) ? firstNumber : secondNumber;
+
+    let total = 0;
+    for (let i = minorNumber; i <= higherNumber; i++) {
+        total += i;
+    }
+
+    return total;
 };
 
 // Do not edit below this line
