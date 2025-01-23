d09237e611c178c2f7241ed9946479f621a40eab
cnuneze <23690843+cnuneze@users.noreply.github.com>
2025-01-23 13:38:33 -0300

    Fix: Add exercise 12 solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(users) {
+    return users.reduce((oldest, user) => {
+        let oldestFinishAge = oldest.yearOfDeath ? oldest.yearOfDeath : new Date().getFullYear();
+        let currentFinishAge = user.yearOfDeath ? user.yearOfDeath : new Date().getFullYear();
 
+        let oldestAge = oldestFinishAge - oldest.yearOfBirth;
+        let currentAge = currentFinishAge - user.yearOfBirth;
+
+        return oldestAge > currentAge ? oldest : user;
+    })
 };
 
 // Do not edit below this line
