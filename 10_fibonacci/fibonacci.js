6032e8e31d68fa2130aa3a9902d7e5e7ccfe943f
cnuneze <23690843+cnuneze@users.noreply.github.com>
2025-01-23 12:52:45 -0300

    Fix: Add exercise 10 solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    const sequence = [];
+    number = parseInt(number);
+    if (number === 0) {
+        return 0;
+    }
+
+    if (number < 0){
+        return "OOPS";
+    }
+    for (let i = 0; i < number; i++) {
+        let previousNumber = sequence[i-2] ? sequence[i-2] : 0;
+        let currentNumber = sequence[i-1] ? sequence[i-1] : 1;
+        
+        let nextNumber = previousNumber + currentNumber;
+        sequence.push(nextNumber);
+    }
+
+    return sequence.pop();
 
 };
 
